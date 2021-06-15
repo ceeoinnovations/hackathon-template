5824fc7ae943375b432d23c57f22083c60516266
Hyejin Im <hyejinim17@gmail.com>
2021-06-15 17:20:05 -0400

    Delete files

--- components/Projects.js
+++ components/Projects.js
@@ -1,4 +1,5 @@
-import MaterialIcon from './MaterialIcon.js';
+import getImageURL, {templateIdFrom} from './Images.js';
+
 export default function Projects(projects){
     return `
     
@@ -14,23 +15,10 @@ export default function Projects(projects){
 }
 
 export function ProjectItems(projects){
-    let getTeaserURL = (image)=>{
-        if (image===""){
-            return 'assets/global/project-placeholder.png';
-        }else if (image.startsWith("http") && image.includes("drive.google.com")){
-            let id = "";
-            const url = new URL(image);
-            id = templateIdFrom(url);
-            // console.log('id: ' + id);
-            return `https://drive.google.com/uc?id=${id}`;
-        }else{
-            return image;
-        }
-    }
 
     return projects.map(d=>`
         <div class="project-box">
-                <img src="${getTeaserURL(d.teaser)}" div class="teaser">
+                <img src="${getImageURL(d.teaser)}" div class="teaser">
                 <div class="info">
                     <div class="project-overview">
                         <div class="project-title">
@@ -49,10 +37,3 @@ export function ProjectItems(projects){
         `).join('');
 }
 
-export function templateIdFrom(url) {
-    url.toString();
-    let match = url.href.match(/([a-z0-9_-]{25,})[$/&?]/i);
-    return match[1];
-    // 1. /([a-z0-9_-]{25,})[$/&?]/i
-    // 2. /\/d\/(.+)\//
-}

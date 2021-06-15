7f5145651e0661454c39d709eec8031e67ceb199
Hyejin Im <hyejinim17@gmail.com>
2021-06-15 16:55:40 -0400

    Delete one image

--- components/About.js
+++ components/About.js
@@ -5,8 +5,7 @@ export default function About(about){
             <h1 class="title">${about[0].name}</h1>
             <p>${about[0].description}</p>
             <div class="project-img">
-            <img src="${about[0].image1}" div class="project-img-holder">
-            <img src="${about[0].image2}" div class="project-img-holder">
+            <img src="${about[0].image}" div class="project-img-holder">
             </div>
             <a href="${about[0].buttonlink}" target="_blank">
                 <button class="button" style="margin-top: 30px; margin-bottom: 50px;">${about[0].buttonlabel}</button>

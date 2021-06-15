36673ee030e0bcc3b50ffdbf975358bf5ebf4c50
Hyejin Im <hyejinim17@gmail.com>
2021-06-15 16:57:21 -0400

    Update csv files

--- components/Projects.js
+++ components/Projects.js
@@ -14,9 +14,23 @@ export default function Projects(projects){
 }
 
 export function ProjectItems(projects){
+    let getTeaserURL = (image)=>{
+        if (image===""){
+            return 'assets/global/project-placeholder.png';
+        }else if (image.startsWith("http") && image.includes("drive.google.com")){
+            let id = "";
+            const url = new URL(image);
+            id = templateIdFrom(url);
+            // console.log('id: ' + id);
+            return `https://drive.google.com/uc?id=${id}`;
+        }else{
+            return image;
+        }
+    }
+
     return projects.map(d=>`
         <div class="project-box">
-                <img src="${d.teaser}" div class="teaser">
+                <img src="${getTeaserURL(d.teaser)}" div class="teaser">
                 <div class="info">
                     <div class="project-overview">
                         <div class="project-title">
@@ -35,3 +49,10 @@ export function ProjectItems(projects){
         `).join('');
 }
 
+export function templateIdFrom(url) {
+    url.toString();
+    let match = url.href.match(/([a-z0-9_-]{25,})[$/&?]/i);
+    return match[1];
+    // 1. /([a-z0-9_-]{25,})[$/&?]/i
+    // 2. /\/d\/(.+)\//
+}

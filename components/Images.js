e86567fcf544cd9b71cf28e0caf97f3c7edda9ee
Hyejin Im <hyejinim17@gmail.com>
2021-07-01 11:50:33 -0400

    Get proper url from google form augo-generated image url

--- components/Images.js
+++ components/Images.js
@@ -3,11 +3,22 @@ export default function GetImageURL(image){
         return '';
         // assets/global/project-placeholder.png
     }else if (image.startsWith("http") && image.includes("drive.google.com")){
-        let id = "";
-        const url = new URL(image);
-        id = templateIdFrom(url);
-        // console.log('id: ' + id);
-        return `https://drive.google.com/uc?id=${id}`;
+        // *--Google Form auto-generated link--*
+        const url = new URL(image); 
+        const urlParams = new URLSearchParams(url.search);
+        if (urlParams.get("id")){
+            return `https://drive.google.com/uc?export=view&id=${urlParams.get("id")}`;
+        }else{
+            const id = image.split('/').slice(-2)[0];// second from last
+            return `https://drive.google.com/uc?export=view&id=${id}`;
+        }
+
+        // *--Google Drive sharable link--*
+        // let id = "";
+        // const url = new URL(image);
+        // id = templateIdFrom(url);
+        // // console.log('id: ' + id);
+        // return `https://drive.google.com/uc?id=${id}`;
     }else{
         return image;
     }

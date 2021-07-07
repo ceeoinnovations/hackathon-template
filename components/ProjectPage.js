b3adc61cf301b5c7da56af2781cc5a596e29c7e4
Hyejin Im <hyejinim17@gmail.com>
2021-07-07 11:44:30 -0400

    Change the layout of details page (2 columns)

--- components/ProjectPage.js
+++ components/ProjectPage.js
@@ -15,30 +15,29 @@ export function ProjectDetail(d){
     return `
     <section id="content" class="project-intro">
         <div class="content-wrapper">
-            <br>
-            <h1 class="title" style="font-size: 3rem; margin-bottom: 10px; text-align: left;">${d.title}</h1>
-            <div class="project-subtitle">
-                ${d.subtitle}
-        </div>
-            
-        <div class="project-tags" style="color: #a7a6a6;">
-            By ${d.authors}
-        </div>
-        
-        <div class="videoWrapper">
-            ${GetEmbedVideo(d.video)}
-        </div>
-        
-        ${ImageItems(d.images)}
-
-        <div class="row">
-            <div class="col-9">
-                <p class="project-desc">
-                    ${d.desc}
-                </p>
-                <br>
+            <div class="row">
+            <div class="col-7">
+                <div class="videoWrapper">
+                    ${GetEmbedVideo(d.video)}
+                </div>
+                ${ImageItems(d.images)}
+            </div>
+            <div class="col-5">
+                <div class="project-info">
+                    <h1 class="title">${d.title}</h1>
+                    <div class="project-subtitle">
+                        ${d.subtitle}
+                    </div>
+                    <div class="project-tags" style="color: #a7a6a6;">
+                        By ${d.authors}
+                    </div>
+                    <p class="project-desc">
+                        ${d.desc}
+                    </p>
+                </div>
             </div>
         </div>
+
     </section>
     `
 }

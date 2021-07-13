666a9c5dbd36c0c924160845fe9454386019eba6
Hyejin Im <hyejinim17@gmail.com>
2021-07-13 15:37:17 -0400

    Add code popup (test)

--- components/About.js
+++ components/About.js
@@ -12,6 +12,9 @@ export default function About(about){
                 <a href="${about[0].buttonlink}" target="_blank">
                     <button class="button" style="margin-top: 30px; margin-bottom: 50px;">${about[0].buttonlabel}</button>
                 </a>
+                <a href="${about[0].resources}" target="_blank">
+                    <button class="button" style="margin-top: 30px; margin-bottom: 50px;">More Resources</button>
+                </a>
         </div>    
     </section>`
 }
--- components/ProjectPage.js
+++ components/ProjectPage.js
@@ -1,6 +1,8 @@
 import GetImageURL, {GetImageArr, GetEmbedVideo} from './Images.js';
 import Navbar from './Navbar.js';
 import Footer from './Footer.js';
+// const hljs = require('highlight.js/lib/common');
+// import hljs from 'highlight.js';
 
 export default function ProjectPage(project, about){
 
@@ -17,13 +19,14 @@ export function ProjectDetail(d){
         <div class="content-wrapper">
             <br>
             <div class="row">
-            <div class="col-7">
+            <div class="col-6">
                 <div class="videoWrapper">
                     ${GetEmbedVideo(d.video)}
                 </div>
                 ${ImageItems(d.images)}
+                
             </div>
-            <div class="col-5">
+            <div class="col-6">
                 <div class="project-info">
                     <h1 class="title">${d.title}</h1>
                     <div class="project-subtitle">
@@ -32,9 +35,27 @@ export function ProjectDetail(d){
                     <p class="project-desc">
                         ${d.desc}
                     </p>
+                   
+                    
+                    <a href="#popup1" target="_blank">
+                        <button class="button" style="margin-top: 30px; margin-bottom: 50px;">View Code</button>
+                    </a>
                     ${AddButton(d.url, d.urlLabel)}
+
                     <div class="project-tags" style="color: #a7a6a6;">
                         By ${d.authors}
+                    </div>
+
+                    ${CodeSnippet(d.code)}
+
+                    <div id="popup1" class="overlay">
+                        <div class="popup">
+                            <h2>Code</h2>
+                            <a class="close" href="#">&times;</a>
+                            <div class="content">
+                                <pre><code class="hljs">${d.code}</code></pre>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -60,6 +81,16 @@ export function AddButton(url, urlLabel){
             <button class="button" style="margin-top: 30px; margin-bottom: 50px;">${urlLabel}</button>
         </a>
 
+        `;
+    }
+}
+
+export function CodeSnippet(code){
+    if (code==="") {
+        return '';
+    }else {
+        return `
+            <pre><code class="hljs">${code}</code></pre>
         `;
     }
 }
